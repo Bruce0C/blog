0e98c520d6ba56d6d31720d7a2a9d009a45e7ffd
Bruce0C <pvlm5xi2@students.codeinstitute.net>
2025-06-05 14:06:37 +0100

    make and update migrations

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,6 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Post(models.Model):
-    """."""
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
@@ -16,3 +15,5 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
